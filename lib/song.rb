c12791a26a333bc623606583f939438166fedcf0
Nii <bnstephens001@gmail.com>
2022-09-06 18:42:51 +0000

    Completed Assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
 class Song
+   
+  extend Findable
+  include Paramable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
